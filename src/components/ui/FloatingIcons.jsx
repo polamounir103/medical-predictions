dabc1fc9abda1aeb10a9b9ec6baf0d7f305ab5c6
polamounir103 <polamounir103@gmail.com>
2024-10-29 19:03:41 +0300

    2nd upload

--- src/components/ui/FloatingIcons.jsx
+++ src/components/ui/FloatingIcons.jsx
@@ -11,7 +11,7 @@ function FloatingIcons() {
   };
 
   return (
-    <div className="fixed bottom-0 right-0">
+    <div className="fixed bottom-10 right-10">
       <ul className="flex justify-end flex-col gap-4 p-5">
         <li>
           <Link to="/chat-ai">
