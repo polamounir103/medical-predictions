6e06491a54aafd5074ba5a7e750031fd65f1e4c0
polamounir103 <polamounir103@gmail.com>
2024-11-04 23:40:11 +0200

    update Header & fotter

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100 mt-28">
+    <footer className="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100">
       <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           <div>
