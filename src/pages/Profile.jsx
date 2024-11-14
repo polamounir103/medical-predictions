29400afa965b759cbad98609a4fd7fc5fade345e
polamounir103 <polamounir103@gmail.com>
2024-11-14 13:13:20 +0200

    update profile & edit profile pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Profile() {
   return (
     <div className="page">
-        Profile
-        <Link to='/edit-profile' >
-        <button>EDIT</button>
-        </Link>
+      <h2 className="m-5">Profile</h2>
+
+      <Link to="/edit-profile">
+        <a className=" border border-sky-950 font-semibold px-10 py-5 text-center rounded-3xl">
+          EDIT
+        </a>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
